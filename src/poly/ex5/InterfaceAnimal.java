8cf2afd906ddb45abef65f40e4712b7ecbc0c9f7
SexyDoGyun <kimdk0903@naver.com>
2025-04-01 15:37:54 +0900

    추상 클래스 study

---
+++ src/poly/ex5/InterfaceAnimal.java
@@ -0,0 +1,8 @@
+package poly.ex5;
+
+public interface InterfaceAnimal {
+    void sound(); //public abstract는 생략가능, 생략을 권장한다.
+    void move(); //public abstract, 인터페이스는 보통 여러곳에서 사용하기 때문에 접근제어자는 public
+}
+
+
